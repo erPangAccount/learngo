06a668ce8cf75db1e05a780e3b188fecb0b520d3
caizhengjun <2585854334@qq.com>
2019-12-07 09:13:00 +0800

    测试struct反射

--- src/test/main.go
+++ src/test/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 type A struct {
 	a int
@@ -18,4 +21,38 @@ func main() {
 	c.A.a = 1
 	c.B.a = 1
 	fmt.Println(c)
+
+	fmt.Println("----------test reflect struct----------------")
+	student := Student{
+		Name:  "Test",
+		Age:   5,
+		Score: 10,
+	}
+	structTest(&student)
+
+	structTest(student)
+}
+
+// stuct refelct 测试
+
+type Student struct {
+	Name  string
+	Age   int
+	Score float32
+}
+
+func (s Student) Print() {
+	fmt.Println(s)
+}
+
+func structTest(a interface{}) {
+	val := reflect.ValueOf(a)
+	t := reflect.TypeOf(a)
+	kd := val.Kind()
+	fmt.Println(val, t, kd)
+
+	if kd != reflect.Ptr && val.Elem().Kind() == reflect.Struct {
+		panic("error")
+	}
+
 }
